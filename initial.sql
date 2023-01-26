d420974e72ef4ed79200430802c48d1e5c509f8b
yuming-long <305248291@qq.com>
2023-01-26 14:04:23 -0500

    add permission to user

--- initial.sql
+++ initial.sql
@@ -4,3 +4,7 @@ CREATE TABLE movies (uid integer PRIMARY KEY, name VARCHAR(50), score DECIMAL(3,
 INSERT INTO movies VALUES(1, 'Forest Gump', 9.9);
 INSERT INTO movies VALUES(2, 'Shining', 8.9);
 SELECT * FROM movies;
+
+CREATE user 'user1'@'localhost' identified by 'Password0!'; 
+GRANT all on testDB.* to 'user1'@'localhost'; 
+ALTER user 'user1'@'localhost' identified with mysql_native_password by 'Password0!';
