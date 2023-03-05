86fcd08f48c8e52a8af95b7f3583933bfcea5c30
Kathy FENG <95158976+Kkathyf@users.noreply.github.com>
2023-03-04 23:36:31 -0500

    delete feature 1

--- test-sample.sql
+++ test-sample.sql
@@ -1,27 +1,22 @@
 # Feature 1:
-SELECT * 
-FROM Movie
-WHERE name = "The Matrix";
-
-# Feature 2:
 SELECT name
 FROM Movie
 WHERE avg_rate > 7.0;
 
-# Feature 3:
+# Feature 2:
 SELECT name
 FROM Movie
 WHERE id in (SELECT mid
 	     FROM Director join Direction on id = did
 	     WHERE first_name = "Hiner" and surname = "Saleem");
 
-# Feature 4:
+# Feature 3:
 SELECT name
 FROM (SELECT name, RANK() OVER (ORDER BY (avg_rate) DESC) as r
       FROM Movie) as T
 WHERE r <= 3;
 
-# Feature 5:
+# Feature 4:
 INSERT INTO Rating VALUES (1, 133093, 6.2, "I like this movie.");
 SELECT *
 FROM Rating;
