796dea4048cb4118a1317c083ec718e03db19c80
SL-continue <j855liu@uwaterloo.ca>
2023-03-06 20:35:21 -0500

    modified:   initial.sql

--- initial.sql
+++ initial.sql
@@ -1,10 +1,4 @@
-CREATE DATABASE testDB;
-USE testDB;
-CREATE TABLE movies (uid integer PRIMARY KEY, name VARCHAR(50), score DECIMAL(3,2));
-INSERT INTO movies VALUES(1, 'Forest Gump', 9.9);
-INSERT INTO movies VALUES(2, 'Shining', 8.9);
-SELECT * FROM movies;
-
+-- Create users
 CREATE user 'user1'@'localhost' identified by 'Password0!'; 
-GRANT all on testDB.* to 'user1'@'localhost'; 
+GRANT all on CS348_MOVIE_DB.* to 'user1'@'localhost'; 
 ALTER user 'user1'@'localhost' identified with mysql_native_password by 'Password0!';
